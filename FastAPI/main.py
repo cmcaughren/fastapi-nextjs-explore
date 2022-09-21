b768fae035f3a645825e8e2712f75afe633b594b
cmcaughren <cmcaughren@gmail.com>
2022-09-21 10:31:36 -0700

    Added a FastAPI endpoint /echo which responds with the same value it is sent.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/echo/{value}")
+async def echo(value):
+    return {"Your Entered Value": value}
