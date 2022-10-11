b7a02ab236d1ea342f528f30f6e719059a18a369
cmcaughren <cmcaughren@gmail.com>
2022-10-10 21:17:13 -0700

    Added a MUI theme to set the site to darkmode.

--- Nextjs/nextjs-blog/pages/_app.js
+++ Nextjs/nextjs-blog/pages/_app.js
@@ -1,5 +1,18 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import '../styles/global.css';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
 export default function App({ Component, pageProps }) {
-   return <Component {...pageProps} />;
+   return (
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />;
+      </ThemeProvider>
+   )
  }
